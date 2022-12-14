e6b434537c3b6f981be697077fd7d2bf11538ba9
lowielow <yihui3302@gmail.com>
2022-12-14 15:52:37 +0800

    Create Day01_part2.kt

---
+++ src/Day01_part2.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    fun part1(input: List<String>): Int {
+        val listOne: MutableList<Int> = mutableListOf<Int>()
+        var currentSum = 0
+        val inputSize: Int = input.size
+        for (i in 0 until inputSize) {
+            if (input[i].isEmpty()) {
+                listOne.add(currentSum)
+                currentSum = 0
+            } else {
+                currentSum += input[i].toInt()
+            }
+        }
+        val listTwo = listOne.sortedDescending()
+        return listTwo[0] + listTwo[1] + listTwo[2]
+    }
+
+    val input = readInput("Day01")
+    part1(input).println()
+
+}
